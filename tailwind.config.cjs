c4c68e91e3c7ff4ddff5e502ffc5612634858ba6
Sanj-sanj <sanjeetu3@gmail.com>
2023-08-24 19:21:05 -0700

    lots of style changes and small updates, added a splash image, shop button, new colors, no dark mode toggle just yet

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "base-dark-purp": "#4C3A51",
+        "base-light-purp": "#774360",
+        "base-pink": "#B25068",
+        "base-yellow": "#E7AB79",
+        "base-dark-navy": "#16213E",
+        "base-light-navy": "#0F3460",
+        "base-purple": "#533483",
+        "base-magenta": "#E94560",
+      },
+    },
   },
   plugins: [],
 };
